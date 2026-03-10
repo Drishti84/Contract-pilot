c6853e97d9ae562424553fb56978095310723e95
Drishti <drishtisingla84@gmail.com>
2026-03-10 08:53:15 +0530

    fixed

---
+++ types/pdf-parse-lib.d.ts
@@ -0,0 +1,15 @@
+declare module 'pdf-parse/lib/pdf-parse.js' {
+  export interface PdfParseResult {
+    numpages: number
+    numrender: number
+    info: unknown
+    metadata: unknown
+    text: string
+    version: string
+  }
+
+  export default function pdfParse(
+    dataBuffer: Buffer,
+    options?: Record<string, unknown>
+  ): Promise<PdfParseResult>
+}
